61178a0ffb3bdbffcf7add7fcc94777ab71cb96d
IakMastro <cse242017102@uniwa.gr>
2020-06-18 20:50:33 +0300

    Final Commit

--- DegreeAverageCalculator.py
+++ DegreeAverageCalculator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from math import isnan
 
 if __name__ == "__main__":
     classes = pd.read_csv("Classes.csv")
@@ -8,14 +8,28 @@ if __name__ == "__main__":
 
     classes_data = np.array(classes)
 
-    gradeSum = 0.0
-    ectsSum = 0.0
+    gradeSum = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    ectsSum = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    totalGradeSum = 0.0
+    totalEctsSum = 0.0
     for class_data in classes_data:
+        if isnan(class_data[2]):
+            continue
+        
         grade = class_data[2]
         ects = class_data[3]
+        semester = class_data[4] - 1
 
-        gradeSum += grade * ects
-        ectsSum += ects
+        gradeSum[semester] += grade * ects
+        ectsSum[semester] += ects
 
-    average = gradeSum / ectsSum
-    print(f"Average is {average}")
+    for semester in range(10):
+        if gradeSum[semester] == 0.0:
+            continue
+
+        totalEctsSum += ectsSum[semester]
+        totalGradeSum += gradeSum[semester]
+        print(f"{semester + 1} Semester average: {gradeSum[semester] / ectsSum[semester]}")
+
+    average = totalGradeSum / totalEctsSum
+    print(f"Total average is: {average}")
