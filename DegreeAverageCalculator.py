b6bd9eb758bf836502a06dbd5178fb74e1145262
cultab <rroarck@gmail.com>
2021-06-08 18:47:49 +0300

    disregard grades < 5, fix for s in semesters
    filter out grades lower than 5, since they don't count towawrds the average
    
    use enumarate over sememesters incase they are not sequencial numbers
    s - 1 was a hack

--- DegreeAverageCalculator.py
+++ DegreeAverageCalculator.py
@@ -3,6 +3,8 @@ import numpy as np
 
 if __name__ == "__main__":
     classes = pd.read_csv("Classes.csv")
+    classes = classes.where(classes['Grade'] >= 5.0)
+
     print(classes)
 
     semesters = np.unique(classes['Semester'])
@@ -10,10 +12,11 @@ if __name__ == "__main__":
     gradeSum = np.zeros(len(semesters))
     ectsSum = np.zeros(len(semesters))
 
-    for s in semesters:
-        gradeSum[s - 1] = np.sum(classes.where(classes['Semester'] == s)['Grade'] * classes.where(classes['Semester'] == s)['ECTS'])
-        ectsSum[s - 1] = np.sum(classes.where(classes['Semester'] == s)['ECTS'])
-        print(f"{s} Semester average: {round(gradeSum[s - 1] / ectsSum[s - 1], 2)}")
+    for i, s in enumerate(semesters):
+        gradeSum[i] = np.sum(classes.where(classes['Semester'] == s)['Grade']
+                             * classes.where(classes['Semester'] == s)['ECTS'])
+        ectsSum[i] = np.sum(classes.where(classes['Semester'] == s)['ECTS'])
+        print(f"{s} Semester average: {round(gradeSum[i] / ectsSum[i], 2)}")
 
     average = np.sum(gradeSum) / np.sum(ectsSum)
     print(f"Total average is: {round(average, 2)}")
